74082763ed3e0576be93b3c41601d0dbe7383c68
RajShraddha <78155340+RajShraddha@users.noreply.github.com>
2022-01-05 23:03:14 +0530

    Wildfire tracker

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import "./styles.css";
+import Map from "./Map";
+import { useState, useEffect } from "react";
 
-export default function App() {
+function App() {
+  const [eventData, setEventData] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true);
+      const res = await fetch("https://eonet.gsfc.nasa.gov/api/v2.1/events");
+      const { events } = await res.json();
+
+      setEventData(events);
+      setLoading(false);
+    };
+
+    fetchEvents();
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <div>{!loading ? <Map eventData={eventData} /> : <h1>loading</h1>}</div>
   );
 }
+export default App;
